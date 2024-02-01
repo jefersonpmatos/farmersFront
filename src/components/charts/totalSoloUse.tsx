fcd35b6361fa3c16c09aa71327b583de5d14d1ee
Jeferson Pereira Matos <92311057+jefersonPMatos@users.noreply.github.com>
2024-02-01 15:28:05 -0300

    fix deploy

--- src/components/charts/totalSoloUse.tsx
+++ src/components/charts/totalSoloUse.tsx
@@ -13,40 +13,6 @@ interface TotalSoloUseChartProps {
   vegetationAreaHectares: number;
 }
 
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  value,
-}: {
-  cx: number;
-  cy: number;
-  midAngle: number;
-  innerRadius: number;
-  outerRadius: number;
-  value: number | string;
-  index: number;
-}) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? "start" : "end"}
-      dominantBaseline="central"
-    >
-      {value}
-    </text>
-  );
-};
-
 export const TotalSoloUseChart: React.FC<TotalSoloUseChartProps> = ({
   cultivableAreaHectares,
   vegetationAreaHectares,
